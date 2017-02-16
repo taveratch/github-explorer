e2176b19996adf57b77b8a55b96390ff33ee17d2
TAWEESOFT <taweesoft@gmail.com>
2017-02-16 08:18:30 +0700

    add material ui style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Route from './route';
 import './index.css';
 
+injectTapEventPlugin(); // event listener for material-ui
+
+const App = () => (
+  <MuiThemeProvider>
+    <Route />
+  </MuiThemeProvider>
+);
+
 ReactDOM.render(
-  <Route />,
+  <App />,
   document.getElementById('root'),
 );
